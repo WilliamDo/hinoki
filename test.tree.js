ff860e501f55fa9b8e32a1c4fb92296fbbd7acc3
William Do <william@ultimaspin.com>
2019-05-11 19:50:37 +0100

    simplify assertions

--- test.tree.js
+++ test.tree.js
@@ -1,34 +1,34 @@
 /* eslint-env node, mocha */
 
 var tree = require('./tree.js');
-var chai = require('chai');
+var assert = require('chai').assert;
 
 describe('tree', function () {
     describe('#depth()', function () {
         it('should return depth of empty tree', function () {
-            chai.assert.equal(tree.depth(null), 0);
+            assert.equal(tree.depth(null), 0);
         });
 
         it('should return depth of single-node tree', function () {
-            chai.assert.equal(tree.depth({}), 1);
+            assert.equal(tree.depth({}), 1);
         });
 
         it('should return depth of two-node tree imbalanced', function () {
-            chai.assert.equal(tree.depth({
+            assert.equal(tree.depth({
                 'left': {},
                 'right': null
             }), 2);
         });
 
         it('should return depth of two-node tree balanced', function () {
-            chai.assert.equal(tree.depth({
+            assert.equal(tree.depth({
                 'left': {},
                 'right': {}
             }), 2);
         });
 
         it('should return depth of 3', function () {
-            chai.assert.equal(tree.depth({
+            assert.equal(tree.depth({
                 'left': {
                     'left': {},
                     'right': {}
